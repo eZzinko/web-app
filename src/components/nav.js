1c530d0b4768341f9329b16a6afdc8c11a4625ea
eZzinko <nimik.daniel@gmail.com>
2021-02-15 14:01:56 +0100

    new NAV

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import logo_img from '../img/logo.svg';
 import { Link } from 'react-router-dom';
 import firebase from '../firebase';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = ({ logged, setLogged }) => {
     const logOut = () => {
@@ -20,13 +21,13 @@ const Nav = ({ logged, setLogged }) => {
             setNavbar(false);
         }
     }
-
     window.addEventListener('scroll', changeBackground);
     // Scroll effect---
+
     return (
         <>
-            <nav className={navbar ? 'navbar active' : 'navbar'}>
-                <div className="nav-bar">
+            <nav className={navbar ? "active" : ""}>
+                <div className="nav">
                     <Link to='/'><h1><img src={logo_img} alt="logo" /></h1></Link>
                     <div className="nav-items">
                         <ul>
@@ -57,11 +58,43 @@ const Nav = ({ logged, setLogged }) => {
                                     </li>
                                     : ""
                             }
-
-
                         </ul>
                     </div>
                     {logged ? <button onClick={logOut} className="button-nav">Odhlásit se</button> : <Link to='/login' className="button-nav">Přihlásit se</Link>}
+                    <input type="checkbox" id="active"></input>
+                    <label for="active" className="menu-btn"><FontAwesomeIcon icon={faBars} /></label>
+                    <div class="wrapper">
+                        <ul>
+                            <li>
+                                <Link to='/'>
+                                    Domů
+                            </Link></li>
+                            <li>
+                                <Link to='/receptar'>
+                                    Receptář
+                            </Link>
+                            </li>
+                            <li>
+                                <Link to='/create'>
+                                    Můj recept
+                            </Link>
+                            </li>
+                            <li>
+                                {
+                                    logged ?
+                                        <li>
+                                            <Link to='/account'>
+                                                Profil
+                                </Link>
+                                        </li>
+                                        : ""
+                                }
+                            </li>
+                            <li>
+                                {logged ? <button onClick={logOut} className="button-nav">Odhlásit se</button> : <Link to='/login' className="button-nav">Přihlásit se</Link>}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </>
